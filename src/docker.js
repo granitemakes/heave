4bfe25de005e947dfd9a90a9fec85e7f524eebcd
Gabe M <gabemeola@gmail.com>
2018-05-27 19:05:06 -0600

    something works :)

--- src/docker.js
+++ src/docker.js
@@ -1,13 +1,23 @@
-const { execSync } = require('child_process');
+const { exec, execSync } = require('child_process');
 
-exports.main = function main() {
-  execSync('docker images', {
-    stdio: 'inherit',
-  })
+function hash(string){
+  return string
+    .split("")
+    .reduce((a,b) => {
+      a=((a<<5)-a)+b.charCodeAt(0);
+      return a&a
+    },0);              
+}
+
+exports.startTraefik = function startTraefik() {
+  exec('traefik');
 }
 
 exports.bootImage = function bootImage(image) {
-  execSync(`docker run -d ${image}`, {
+  const imageHash = hash(image);
+  execSync(`docker run -d --label traefik.frontend.rule=Host:${imageHash}.localhost ${image}`, {
     stdio: 'inherit'
   })
+
+  return imageHash;
 }
--- src/index.js
+++ src/index.js
@@ -9,9 +9,10 @@ const app = express();
 app.post('/add', (req, res) => {
   console.log(req.query.image)
   res.writeHead(200);
-  res.write(`Booting ${req.query.image}...`)
-  docker.bootImage(req.query.image)
+  res.write(`Booting ${req.query.image}...\n`)
+  const hostHash = docker.bootImage(req.query.image)
   res.write('Done.')
+  res.write(`${hostHash}.localhost`)
   res.end();
 })
 app.use('/', (req, res) => {
@@ -24,5 +25,5 @@ app.use('/', (req, res) => {
 http.createServer(app)
   .listen(PORT, () => {
     console.log(`Server is up and running at port: ${PORT}`)
-    // docker();
+    docker.startTraefik();
   })
